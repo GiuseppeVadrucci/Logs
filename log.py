a4f3c487f63867244e2fe9b3f2775b03d992fbd1
giuseppe90 <givadrucci@gmail.com>
2018-02-24 21:54:46 +0100

    Create log.py

---
+++ log.py
@@ -0,0 +1,26 @@
+import time
+def follow(thefile):
+    thefile.seek(0,2)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+
+import string
+ip = ''
+
+file1 = open("/../../nginx/access.log","r")
+file2 = open("/../log.txt","w")
+while True:
+
+	for line in follow(file1):
+
+                line1 = line.split(" ")
+
+                result = ''.join([i for i in line1[0] if i.isdigit() ])
+                if result != ip:
+                        file2.write(line)
+
